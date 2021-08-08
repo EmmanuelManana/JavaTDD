8415dea8e522483fab611c6d395ecc1e216b03f5
emmanuel <emmanuel.manana@heycarter.co.za>
2021-08-08 16:47:25 +0200

    first test cases

---
+++ src/main/java/co/za/emanana/incubayte/StringCalculator.java
@@ -0,0 +1,8 @@
+package co.za.emanana.incubayte;
+
+public class StringCalculator {
+
+    public int Add(String numbers){
+        return 0;
+    }
+}
---
+++ src/test/java/co/za/emanana/incubayte/StringCalculatorTest.java
@@ -0,0 +1,35 @@
+package co.za.emanana.incubayte;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringCalculatorTest {
+    StringCalculator stringCalculator;
+
+//    @BeforeAll
+//    static void init(){
+//        System.out.println("Nothing here");
+//    }
+
+    @BeforeEach
+    void init(){
+        stringCalculator = new StringCalculator();
+    }
+
+    @Nested
+    @DisplayName("Testing stringCalculator")
+    class returnSum{
+        @Test
+        @DisplayName("Testing 0 to 3 numbers")
+        void testSumToThree(){
+                assertAll(  () -> assertEquals(0, stringCalculator.Add(""), "should return a 0"),
+                            () -> assertEquals(1, stringCalculator.Add("1"), "should return a 0"),
+                            () -> assertEquals(3, stringCalculator.Add("1,3"), "should return a 0"));
+
+        }
+    }
+
+
+}
