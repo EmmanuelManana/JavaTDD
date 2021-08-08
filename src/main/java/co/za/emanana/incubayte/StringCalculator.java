ce263871fb87088943b8de90ee57b804704f7d6c
emmanuel <emmanuel.manana@heycarter.co.za>
2021-08-08 17:05:42 +0200

    test two

--- src/main/java/co/za/emanana/incubayte/StringCalculator.java
+++ src/main/java/co/za/emanana/incubayte/StringCalculator.java
@@ -2,7 +2,17 @@ package co.za.emanana.incubayte;
 
 public class StringCalculator {
 
-    public int Add(String numbers){
+//    public static void main(String[] args) {
+//        Add("1\n2,3");
+//    }
+
+
+    public  int Add(String numbers){
+
+        if (numbers.contains("\n")){
+           numbers =  numbers.replaceAll("\n", ",,");
+        }
+
         return numbers.length();
     }
 }
--- src/test/java/co/za/emanana/incubayte/StringCalculatorTest.java
+++ src/test/java/co/za/emanana/incubayte/StringCalculatorTest.java
@@ -25,8 +25,9 @@ public class StringCalculatorTest {
         @DisplayName("Testing 0 to 3 numbers")
         void testSumToThree(){
                 assertAll(  () -> assertEquals(0, stringCalculator.Add(""), "should return a 0"),
-                            () -> assertEquals(1, stringCalculator.Add("1"), "should return a 0"),
-                            () -> assertEquals(3, stringCalculator.Add("1,3"), "should return a 0"));
+                            () -> assertEquals(1, stringCalculator.Add("1"), "should return a 1"),
+                            () -> assertEquals(3, stringCalculator.Add("1,3"), "should return a 3"),
+                            () -> assertEquals(6, stringCalculator.Add("1\n2,3"), "should return a 6"));
 
         }
     }
