8ef18a227e7c1cc1bf22d6b66f3868cd5e530e1c
emmanuel <emmanuel.manana@heycarter.co.za>
2021-08-09 13:11:22 +0200

    exceptions test

--- src/main/java/co/za/emanana/incubayte/StringCalculator.java
+++ src/main/java/co/za/emanana/incubayte/StringCalculator.java
@@ -1,14 +1,40 @@
 package co.za.emanana.incubayte;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
 
-    public int Add(String numbers){
-        if (numbers.contains(";")){
+    public static void main(String[] args) {
+        String testInput = "-5, 8, 0";
+        Add(testInput);
+    }
+
+
+    public static int Add(String numbers){
+
+        if (numbers.contains("-")){
+            String[] numbersArray = new String[numbers.split(",").length];
+            numbersArray = numbers.split(",");
+            List getNegatives = new ArrayList();
+            for (String item : numbersArray){
+                int currentNumber = Integer.parseInt(item.trim());
+
+                if (currentNumber < 0){
+                    getNegatives.add(currentNumber);
+                }
+
+            }
+            throw new RuntimeException("negatives note allowed , negatives found :" +  getNegatives);
+        }
+        else if (numbers.contains(";")){
             return numbers.split(";").length;
         }
         else if (numbers.contains("\n")){
            numbers =  numbers.replaceAll("\n", ",,");
         }
+
 
         return numbers.length();
     }
--- src/test/java/co/za/emanana/incubayte/StringCalculatorTest.java
+++ src/test/java/co/za/emanana/incubayte/StringCalculatorTest.java
@@ -2,8 +2,7 @@ package co.za.emanana.incubayte;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
     StringCalculator stringCalculator;
@@ -35,6 +34,12 @@ public class StringCalculatorTest {
         @DisplayName("Testing ; as delimeters")
         void delimitersTest(){
             assertEquals(3, stringCalculator.Add("//;\n1;2"), "should return a 3");
+        }
+
+        @Test
+        @DisplayName("Testing negatives")
+        void negativeNumbers(){
+            assertThrows(RuntimeException.class, () ->stringCalculator.Add("//;\n1;-2"), "cannot process negative numbers");
         }
     }
 
